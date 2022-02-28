2096576140022b3dd46f68fc4d05a997f9992558
Mirko Da Corte <mirko@etherna.io>
2022-02-28 19:12:42 +0100

    first truffle config

--- src/EIT/contracts/EIT.sol
+++ src/EIT/contracts/EIT.sol
@@ -1,19 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.25 <0.7.0;
+pragma solidity >=0.8.12 <0.9.0;
 
-import "./ConvertLib.sol";
-
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
-
-contract MetaCoin {
+contract EIT {
 	mapping (address => uint) balances;
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	constructor() public {
+	constructor() {
 		balances[tx.origin] = 10000;
 	}
 
@@ -23,10 +16,6 @@ contract MetaCoin {
 		balances[receiver] += amount;
 		emit Transfer(msg.sender, receiver, amount);
 		return true;
-	}
-
-	function getBalanceInEth(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
 	}
 
 	function getBalance(address addr) public view returns(uint) {
--- src/EIT/truffle-config.js
+++ src/EIT/truffle-config.js
@@ -33,6 +33,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
+      version: "0.8.12",
     }
   }
 }
